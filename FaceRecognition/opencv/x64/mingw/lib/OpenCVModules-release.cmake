5b5eb75225ea0612b17e23e60ea8280700ba8fa0
冯乃旺 <16403124+feng-naiwang@user.noreply.gitee.com>
2026-09-06 00:54:25 +0800

    20260906

---
+++ FaceRecognition/opencv/x64/mingw/lib/OpenCVModules-release.cmake
@@ -0,0 +1,119 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "opencv_core" for configuration "Release"
+set_property(TARGET opencv_core APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_core PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_core4100.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_core4100.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_core )
+list(APPEND _cmake_import_check_files_for_opencv_core "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_core4100.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_core4100.dll" )
+
+# Import target "opencv_flann" for configuration "Release"
+set_property(TARGET opencv_flann APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_flann PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_flann4100.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_flann4100.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_flann )
+list(APPEND _cmake_import_check_files_for_opencv_flann "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_flann4100.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_flann4100.dll" )
+
+# Import target "opencv_imgproc" for configuration "Release"
+set_property(TARGET opencv_imgproc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_imgproc PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_imgproc4100.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_imgproc4100.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_imgproc )
+list(APPEND _cmake_import_check_files_for_opencv_imgproc "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_imgproc4100.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_imgproc4100.dll" )
+
+# Import target "opencv_photo" for configuration "Release"
+set_property(TARGET opencv_photo APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_photo PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_photo4100.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_photo4100.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_photo )
+list(APPEND _cmake_import_check_files_for_opencv_photo "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_photo4100.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_photo4100.dll" )
+
+# Import target "opencv_features2d" for configuration "Release"
+set_property(TARGET opencv_features2d APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_features2d PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_features2d4100.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_features2d4100.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_features2d )
+list(APPEND _cmake_import_check_files_for_opencv_features2d "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_features2d4100.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_features2d4100.dll" )
+
+# Import target "opencv_imgcodecs" for configuration "Release"
+set_property(TARGET opencv_imgcodecs APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_imgcodecs PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_imgcodecs4100.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_imgcodecs4100.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_imgcodecs )
+list(APPEND _cmake_import_check_files_for_opencv_imgcodecs "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_imgcodecs4100.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_imgcodecs4100.dll" )
+
+# Import target "opencv_videoio" for configuration "Release"
+set_property(TARGET opencv_videoio APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_videoio PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_videoio4100.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_videoio4100.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_videoio )
+list(APPEND _cmake_import_check_files_for_opencv_videoio "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_videoio4100.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_videoio4100.dll" )
+
+# Import target "opencv_calib3d" for configuration "Release"
+set_property(TARGET opencv_calib3d APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_calib3d PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_calib3d4100.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_calib3d4100.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_calib3d )
+list(APPEND _cmake_import_check_files_for_opencv_calib3d "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_calib3d4100.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_calib3d4100.dll" )
+
+# Import target "opencv_highgui" for configuration "Release"
+set_property(TARGET opencv_highgui APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_highgui PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_highgui4100.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_highgui4100.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_highgui )
+list(APPEND _cmake_import_check_files_for_opencv_highgui "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_highgui4100.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_highgui4100.dll" )
+
+# Import target "opencv_objdetect" for configuration "Release"
+set_property(TARGET opencv_objdetect APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_objdetect PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_objdetect4100.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_objdetect4100.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_objdetect )
+list(APPEND _cmake_import_check_files_for_opencv_objdetect "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_objdetect4100.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_objdetect4100.dll" )
+
+# Import target "opencv_face" for configuration "Release"
+set_property(TARGET opencv_face APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_face PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_face4100.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_face4100.dll"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_face )
+list(APPEND _cmake_import_check_files_for_opencv_face "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_face4100.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_face4100.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
